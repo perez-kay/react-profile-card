3cbc5a493396b651b8703a0d7982468c13464a04
Kayla Perez <perezkay@uw.edu>
2024-06-12 17:27:33 -0700

    refacted components to be reusable

--- src/index.js
+++ src/index.js
@@ -3,45 +3,51 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App() {
+  return <ProfileCard />;
+}
+
+function ProfileCard() {
   return (
     <div className="card">
-      <img src="/profile-picture.jpeg" alt="profile" />
-      <div className="profile">
-        <Profile />
-        <SkillsList />
-      </div>
+      <Profile
+        name="Kayla Perez"
+        bio="An aspiring Full-Stack Developer working on cool projects. I have a
+          strong background in Python as it's my favorite language to program
+          with. However, learning React and refreshing my JavaScript skills has
+          been quite fun! When I'm not coding, I love to draw, pet my cat, and
+          drink coffee."
+      />
     </div>
   );
 }
 
-function Profile() {
+function Profile(props) {
   return (
     <>
-      <h1>Kayla Perez</h1>
-      <p>
-        Just an aspiring Web Developer working on cool projects. BS in Computer
-        Science, June 2022. When I'm not coding, I love to draw, pet my cat, and
-        drink coffee!
-      </p>
-    </>
-  );
-}
-
-function SkillsList() {
-  return (
-    <>
-      <Skill name="HTML/CSS" />
-      <Skill name="Python" />
-      <Skill name="JavaScript" />
-      <Skill name="React" />
-      <Skill name="SQL" />
-      <Skill name="Linux" />
+      <img src="/profile-picture.jpeg" alt="profile" />
+      <div className="profile">
+        <h1>{props.name}</h1>
+        <hr />
+        <p>{props.bio}</p>
+        <h2>Skills</h2>
+        <hr />
+        <Skill name="HTML/CSS" color="#fab170" />
+        <Skill name="Python" color="#70b7fa" />
+        <Skill name="JavaScript" color="#fae570" />
+        <Skill name="React" color="#9ffcf3" />
+        <Skill name="SQL" color="#fc9fba" />
+        <Skill name="Linux" color="#d1b3fc" />
+      </div>
     </>
   );
 }
 
 function Skill(props) {
-  return <p style={{ color: props.color }}>{props.name}</p>;
+  return (
+    <p className="skill" style={{ backgroundColor: props.color }}>
+      {props.name}
+    </p>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
